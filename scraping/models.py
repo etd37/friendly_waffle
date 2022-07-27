68be9c78828d57ad67d9841aad599e0d181e9ae0
ivbojar <ivbojar@gmail.com>
2022-07-27 22:00:15 +0300

    loop and bootstrap

--- scraping/models.py
+++ scraping/models.py
@@ -5,12 +5,38 @@ from django.db import models
 
 class City(models.Model):
     name = models.CharField(max_length=50,
-                            verbose_name='City name')
-    slug = models.CharField(max_length=50, blank=True)
+                            verbose_name='City name',
+                            unique=True)
 
     class Meta:
-        verbose_name= 'City Name'
-        verbose_name_plural = "Names"
+        verbose_name = 'City Name'
+        verbose_name_plural = "City Names"
 
     def __str__(self):
         return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Programming Language',
+                            unique=True)
+
+    class Meta:
+        verbose_name = 'Programming Language'
+        verbose_name_plural = 'Programming Languages'
+
+    def __str__(self):
+        return self.name
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250)
+    company = models.CharField(max_length=250)
+    description = models.TextField()
+    city = models.ForeignKey('City', on_delete=models.CASCADE)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE)
+    timestamp = models.DateField(auto_now_add=True)     #automatically adds date when enters DB
+
+    def __str__(self):
+        return self.title
